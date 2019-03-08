4759932d144c0288a417a3adf1d292ce525b4f19
Lauren Rose <larose@radford.edu>
2019-03-07 23:50:24 -0500

    add recursive alg for match checking

--- src/CrushController.java
+++ src/CrushController.java
@@ -16,38 +16,53 @@ class CrushController {
 
     void crushButtonClicked(int x, int y) {
         checkForMatches(x, y);
+        for (int i = 0; i < matchesFound.size(); i++) {
+            System.out.println(matchesFound.get(i).toString());
+        }
     }
 
     private void checkForMatches(int initX, int initY) {
         // check 4 around. if match, add to stack. once all four checked,
         // pop the top of the stack, add popped to AL, and recurse
+        int[] current = {initX, initY};
+        matchesFound.add(current);
         int comparisonSymbol = getSymbolByLocation(initX, initY);
         int leftX = initX == 0 ? -1 : initX - 1;
         int rightX = initX == 7 ? -1 : initX + 1;
         int upY = initY == 0 ? -1 : initY - 1;
         int downY = initY == 7 ? -1 : initY + 1;
-        while (foundMatch) {
+        //while (foundMatch) {
             if (leftX != -1 && getSymbolByLocation(leftX, initY) == comparisonSymbol) {
-                int[] temp = {leftX, initY};
-                matchStack.push(temp);
-                foundMatch = true;
+                int[] tempL = {leftX, initY};
+                matchStack.push(tempL);
+                if (!isInListAlready(tempL)) checkForMatches(leftX, initY);
             }
             if (upY != -1 && getSymbolByLocation(initX, upY) == comparisonSymbol) {
-                int[] temp = {initX, upY};
-                matchStack.push(temp);
-                foundMatch = true;
+                int[] tempU = {initX, upY};
+                matchStack.push(tempU);
+                if (!isInListAlready(tempU)) checkForMatches(initX, upY);
             }
             if (rightX != -1 && getSymbolByLocation(rightX, initY) == comparisonSymbol) {
-                int[] temp = {rightX, initY};
-                matchStack.push(temp);
-                foundMatch = true;
+                int[] tempR = {rightX, initY};
+                matchStack.push(tempR);
+                if (!isInListAlready(tempR)) checkForMatches(rightX, initY);
             }
             if (downY != -1 && getSymbolByLocation(initX, downY) == comparisonSymbol) {
-                int[] temp = {initX, downY};
-                matchStack.push(temp);
-                foundMatch = true;
+                int[] tempD = {initX, downY};
+                matchStack.push(tempD);
+                if (!isInListAlready(tempD)) checkForMatches(initX, downY);
+            }
+        //}
+    }
+
+    private boolean isInListAlready(int[] arr) {
+        for (int i = 0; i < matchesFound.size(); i++) {
+            if (matchesFound.get(i)[0] == arr[0] && matchesFound.get(i)[1] == arr[1]) {
+                System.out.println("match");
+                return true;
             }
         }
+        return false;
     }
 
     int getSymbolByLocation(int row, int col) {
